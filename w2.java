8702c8de061630a81c6e0874566f1414651ea6aa
srinivas559 <srinivasravuri10@gmail.com>
2020-02-10 23:36:36 +0530

    added to branch

---
+++ w2.java
@@ -0,0 +1,10 @@
+ class WrapperExample2
+{    
+public static void main(String args[])
+{    
+Integer a=new Integer(3);    
+int i=a.intValue();
+  int j=a;   //unboxing;
+    		System.out.println(a+" "+i+" "+j);    
+}
+}    
